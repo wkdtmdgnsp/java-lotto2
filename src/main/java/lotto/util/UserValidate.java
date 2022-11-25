f9d4adbb4d796bfed04ef279059f8c7947586b95
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-25 17:16:44 +0900

    feat(lotto) : 구입 금액 숫자가 아닌 경우 예외 처리 기능 구현

--- src/main/java/lotto/util/UserValidate.java
+++ src/main/java/lotto/util/UserValidate.java
@@ -1,5 +1,13 @@
 package lotto.util;
 
 public class UserValidate {
-
+    public int buyNumberException(String inputMoney) {
+        try {
+            int money = Integer.parseInt(inputMoney);
+            return money;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자만 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+    }
 }
