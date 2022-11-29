9bfc4f371a479a588eb1f53064413d6de176e03c
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-29 18:44:58 +0900

    refactor(lotto) : 당첨 통계 콤마 나오게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ import lotto.model.Lotto;
 import lotto.model.LottoResult;
 import lotto.model.Rank;
 
+import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,14 +20,15 @@ public class OutputView {
     }
 
     public void printWinning(LottoResult lottoResult) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
         Map<Rank, Integer> rank = new HashMap<>(lottoResult.getResult());
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println(Rank.WIN5.getMatchCount() +"개 일치 (" +Rank.WIN5.getWinningMoney() +"원) - " +rank.get(Rank.WIN5) +"개");
-        System.out.println(Rank.WIN4.getMatchCount() +"개 일치 (" +Rank.WIN4.getWinningMoney() +"원) - " +rank.get(Rank.WIN4) +"개");
-        System.out.println(Rank.WIN3.getMatchCount() +"개 일치 (" +Rank.WIN3.getWinningMoney() +"원) - " +rank.get(Rank.WIN3) +"개");
-        System.out.println(Rank.WIN2.getMatchCount() +"개 일치, 보너스 볼 일치 (" +Rank.WIN2.getWinningMoney() +"원) - " +rank.get(Rank.WIN2) +"개");
-        System.out.println(Rank.WIN1.getMatchCount() +"개 일치 (" +Rank.WIN1.getWinningMoney() +"원) - " +rank.get(Rank.WIN1) +"개");
+        System.out.println(Rank.WIN5.getMatchCount() +"개 일치 (" +numberFormat.format(Rank.WIN5.getWinningMoney()) +"원) - " +rank.get(Rank.WIN5) +"개");
+        System.out.println(Rank.WIN4.getMatchCount() +"개 일치 (" +numberFormat.format(Rank.WIN4.getWinningMoney()) +"원) - " +rank.get(Rank.WIN4) +"개");
+        System.out.println(Rank.WIN3.getMatchCount() +"개 일치 (" +numberFormat.format(Rank.WIN3.getWinningMoney()) +"원) - " +rank.get(Rank.WIN3) +"개");
+        System.out.println(Rank.WIN2.getMatchCount() +"개 일치, 보너스 볼 일치 (" +numberFormat.format(Rank.WIN2.getWinningMoney()) +"원) - " +rank.get(Rank.WIN2) +"개");
+        System.out.println(Rank.WIN1.getMatchCount() +"개 일치 (" +numberFormat.format(Rank.WIN1.getWinningMoney()) +"원) - " +rank.get(Rank.WIN1) +"개");
     }
 
     public void printRate(double rate) {
